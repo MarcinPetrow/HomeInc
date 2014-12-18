7562e4e9b6ddc143e8094090ec9b13afbb7b39ad
Marcin Petrów <MarcinPetrow@gmail.com>
2014-12-18 22:34:34 +0100

    Add icons resources and basic iconbutton - first touch

---
+++ HomeIncClient/Views/Controls/IconButton.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Media;
+
+namespace HomeIncClient.Views.Controls
+{
+
+    public partial class IconButton
+    {
+
+        public int IconSize { get; set; }
+        public int Size { get; set; }
+
+        public SolidColorBrush IconColor { get; set; }
+
+
+        public IconButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
